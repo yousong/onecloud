bcded663d90746a35aed97a151f17165dbbd2a88
Yousong Zhou <zhouyousong@yunionyun.com>
2020-03-18 18:23:06 +0800

    azure: use AzureClientConfig

--- pkg/multicloud/azure/provider/provider.go
+++ pkg/multicloud/azure/provider/provider.go
@@ -113,7 +113,8 @@ func parseAccount(account, secret string) (tenantId string, appId string, appKey
 
 func (self *SAzureProviderFactory) GetProvider(cfg cloudprovider.ProviderConfig) (cloudprovider.ICloudProvider, error) {
 	tenantId, appId, appKey, subId := parseAccount(cfg.Account, cfg.Secret)
-	if client, err := azure.NewAzureClient(cfg.Id, cfg.Name, cfg.URL, tenantId, appId, appKey, subId, false); err != nil {
+	clientCfg := azure.NewAzureClientConfig(cfg.URL, tenantId, appId, appKey).SubscriptionId(subId)
+	if client, err := azure.NewAzureClient(clientCfg); err != nil {
 		return nil, err
 	} else {
 		return &SAzureProvider{
